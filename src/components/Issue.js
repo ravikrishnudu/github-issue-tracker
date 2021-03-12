f16ce8cded4d55d8db01d1cc705e3a49717a5d29
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-03-12 18:54:32 +0530

    single component

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -56,14 +56,11 @@ class Issue extends Component {
               <button className="open-button">Open</button>
               <div className="user-details">
                 <span className="issue-user-login">{issue.user.login}</span>
-                {/* <span className="issue-opend-time">
-                  {" "}
-                  opened this issue yesterday{"   "}
-                </span> */}
-                <div>
-                  openedthis issue{" "}
+
+                <span className="issue-opend-time">
+                  opened this issue{" "}
                   {formatDistance(Date.now(), parseISO(issue.updated_at))} ago
-                </div>
+                </span>
                 <span className="issue-comments">
                   {issue.comments} comments
                 </span>
@@ -74,37 +71,48 @@ class Issue extends Component {
             <div className="left-container">{issue.body}</div>
             <div className="right-container">
               <div>
-                <div>
-                  <div>Assignees {issue.assignees}</div>
-                  <span>No one assigned</span>
+                <div className="element-container">
+                  <div className="element-title">
+                    Assignees {issue.assignees}
+                  </div>
+                  <span className="element-child">No one assigned</span>
                 </div>
-                <div>
-                  Labels
+                <div className="element-container-1">
+                  <div className="element-title">Labels</div>
                   {issue.labels.map((label) => (
-                    <span style={{ backgroundColor: `#${label.color}` }}>
+                    <span
+                      className="label-component"
+                      style={{ backgroundColor: `#${label.color}` }}
+                    >
                       {label.name}
                     </span>
                   ))}
                 </div>
-                <div>
-                  <div>Projects</div>
-                  <span>None yet</span>
+                <div className="element-container-1">
+                  <div className="element-title">Projects</div>
+                  <span className="element-child">None yet</span>
                 </div>
-                <div>
-                  <div>Milestone</div>
-                  <span>None Milestone</span>
+                <div className="element-container-1">
+                  <div className="element-title">Milestone</div>
+                  <span className="element-child">None Milestone</span>
                 </div>
-                <div>
-                  <div>Linked pull requests</div>
-                  <p>Sucessfully merging a pull request may close this issue</p>
-                  <span>None yet</span>
+                <div className="element-container-1">
+                  <div className="element-title">Linked pull requests</div>
+                  <p className="element-child">
+                    Sucessfully merging a pull request may close this issue
+                  </p>
+                  <span className="element-child">None yet</span>
                 </div>
               </div>
-              <div>
-                <span>Notifications</span>
-                <span>Customize</span>
-                <button>Subscribe</button>
-                <p>You’re not receiving notifications from this thread.</p>
+              <div className="element-container-1">
+                <div className="child-elements">
+                  <span className="element-title"> Notifications</span>
+                  <span className="element-child">Customize</span>
+                </div>
+                <button className="sub-button">Subscribe</button>
+                <p className="element-child">
+                  You’re not receiving notifications from this thread.
+                </p>
               </div>
             </div>
           </div>
