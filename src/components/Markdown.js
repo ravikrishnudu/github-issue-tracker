8c2865cb19b6f20e428b1921d37eb738050c8cc3
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-03-13 22:22:30 +0530

    formating text in body

---
+++ src/components/Markdown.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import marked from "marked";
+import "./Markdown.css";
+
+export default class Markdown extends Component {
+  render() {
+    const { body } = this.props;
+    console.log(body);
+    const html = marked(body);
+    return (
+      <>
+        <div className="body" dangerouslySetInnerHTML={{ __html: html }} />
+      </>
+    );
+  }
+}
