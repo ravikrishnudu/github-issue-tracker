9f34da3f903bafd4ead1c304b7bbdd3b2a01c543
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-03-17 02:21:08 +0530

    destructuring

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -8,21 +8,28 @@ import styles from "./Issue.module.css";
 export default class CommentContainer extends Component {
   render() {
     const { issue } = this.props;
+    const { body, updated_at, user } = issue;
     return (
       <div>
+        {/* <img
+          className={styles.userImage}
+          src={issue.user.avatar_url}
+          alt="user profile logo"
+        />
+        <div className={styles.leftArrow}> */}
         <div className={styles.issueCommentHead}>
-          <div className={styles.issueUserLogin}>{issue.user.login}</div>
+          <div className={styles.issueUserLogin}>{user.login}</div>
           <div className={styles.issueOpendTime}>
-            commented {formatDistance(Date.now(), parseISO(issue.updated_at))}{" "}
-            ago
+            commented {formatDistance(Date.now(), parseISO(updated_at))} ago
           </div>
         </div>
         <div>
           <div className={styles.leftContainer}>
-            <Markdown body={issue.body} />
+            <Markdown body={body} />
           </div>
         </div>
       </div>
+      // </div>s
     );
   }
 }
