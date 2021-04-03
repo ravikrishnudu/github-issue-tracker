4b847221609cfb400186a79da742a93bd60f1eb9
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-04-03 18:42:51 +0530

    edit & delete btns and delete  btn functionality

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,12 +1,46 @@
 import React from "react";
 import { formatDistance, parseISO } from "date-fns";
+import { Listbox, ListboxOption } from "@reach/listbox";
+import "@reach/listbox/styles.css";
 
 import Markdown from "./Markdown";
 import "./Markdown.css";
 import styles from "./CommentContainer.module.css";
 
-export default function CommentContainer({ body, updated_at, user }) {
-  // console.log(this.props);
+export default function CommentContainer(props) {
+  const { id, body, updated_at, user, fetchComments } = props;
+  console.log(id);
+
+  function handleDelete() {
+    const deleteComment = {
+      owner: "ravikrishnudu",
+      repo: "git",
+      comment_id: id,
+    };
+
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+      },
+      body: JSON.stringify(deleteComment),
+    };
+
+    fetch(
+      `https://api.github.com/repos/ravikrishnudu/git/issues/comments/${id}`,
+      options
+    )
+      // .then((response) => response.json())
+      .then((data) => {
+        console.log("Succes:", data);
+        fetchComments();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+
   return (
     <div className={styles.commentContainer}>
       <img
@@ -17,11 +51,31 @@ export default function CommentContainer({ body, updated_at, user }) {
       <div className={styles.leftArrow}>
         <div className={styles.commentBody}>
           <div className={styles.issueCommentHead}>
-            <div className={styles.userLogin}>{user.login} </div>
-            <div>
-              commented {formatDistance(Date.now(), parseISO(updated_at))} ago
+            <div className={styles.issueCommentDetails}>
+              <div className={styles.userLogin}>{user.login} </div>
+              <div>
+                commented {formatDistance(Date.now(), parseISO(updated_at))} ago
+              </div>
+            </div>
+            <div className={styles.listBox}>
+              <button>Edit</button>
+              <button onClick={handleDelete}>delete</button>
+
+              <Listbox defaultValue="ravi">
+                <ListboxOption value="ravi">....</ListboxOption>
+                <ListboxOption className={styles.listBoxOption} value="edit">
+                  Edit
+                </ListboxOption>
+                <ListboxOption className={styles.listBoxOption} value="hide">
+                  Hide
+                </ListboxOption>
+                <ListboxOption className={styles.listBoxOption} value="delete">
+                  Delete
+                </ListboxOption>
+              </Listbox>
             </div>
           </div>
+
           <div>
             <div className={styles.leftContainer}>
               <Markdown body={body} />
