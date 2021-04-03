2733527bd42c2496305188e986416c26ef1e4d00
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2021-04-03 23:46:31 +0530

    changed to class component

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 import { formatDistance, parseISO } from "date-fns";
 import { Listbox, ListboxOption } from "@reach/listbox";
 import "@reach/listbox/styles.css";
 
+import BodyComposer from "./BodyComposer";
 import Markdown from "./Markdown";
 import "./Markdown.css";
 import styles from "./CommentContainer.module.css";
 
-export default function CommentContainer(props) {
-  const { id, body, updated_at, user, fetchComments } = props;
-  console.log(id);
-
-  function handleDelete() {
+export default class CommentContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { editComment: false };
+  }
+  handleDelete = () => {
+    const { id, fetchComments } = this.props;
     const deleteComment = {
       owner: "ravikrishnudu",
       repo: "git",
@@ -31,7 +34,6 @@ export default function CommentContainer(props) {
       `https://api.github.com/repos/ravikrishnudu/git/issues/comments/${id}`,
       options
     )
-      // .then((response) => response.json())
       .then((data) => {
         console.log("Succes:", data);
         fetchComments();
@@ -39,50 +41,58 @@ export default function CommentContainer(props) {
       .catch((error) => {
         console.error("Error:", error);
       });
-  }
+  };
+  handleEdit = () => {};
+  render() {
+    const { body, updated_at, user } = this.props;
+    return (
+      <div className={styles.commentContainer}>
+        <img
+          className={styles.userImage}
+          src={user.avatar_url}
+          alt="user profile logo"
+        />
+        <div className={styles.leftArrow}>
+          <div className={styles.commentBody}>
+            <div className={styles.issueCommentHead}>
+              <div className={styles.issueCommentDetails}>
+                <div className={styles.userLogin}>{user.login} </div>
+                <div>
+                  commented {formatDistance(Date.now(), parseISO(updated_at))}{" "}
+                  ago
+                </div>
+              </div>
+              <div className={styles.listBox}>
+                <button onClick={this.handleEdit}>Edit</button>
+                {/* <button onClick={this.handleDelete}>delete</button> */}
 
-  return (
-    <div className={styles.commentContainer}>
-      <img
-        className={styles.userImage}
-        src={user.avatar_url}
-        alt="user profile logo"
-      />
-      <div className={styles.leftArrow}>
-        <div className={styles.commentBody}>
-          <div className={styles.issueCommentHead}>
-            <div className={styles.issueCommentDetails}>
-              <div className={styles.userLogin}>{user.login} </div>
-              <div>
-                commented {formatDistance(Date.now(), parseISO(updated_at))} ago
+                <Listbox defaultValue="ravi">
+                  <ListboxOption value="ravi">....</ListboxOption>
+                  <ListboxOption className={styles.listBoxOption} value="edit">
+                    Edit
+                  </ListboxOption>
+                  <ListboxOption className={styles.listBoxOption} value="hide">
+                    Hide
+                  </ListboxOption>
+                  <ListboxOption
+                    onClick={this.handleDelete}
+                    className={styles.listBoxOption}
+                    value="delete"
+                  >
+                    Delete
+                  </ListboxOption>
+                </Listbox>
               </div>
             </div>
-            <div className={styles.listBox}>
-              <button>Edit</button>
-              <button onClick={handleDelete}>delete</button>
 
-              <Listbox defaultValue="ravi">
-                <ListboxOption value="ravi">....</ListboxOption>
-                <ListboxOption className={styles.listBoxOption} value="edit">
-                  Edit
-                </ListboxOption>
-                <ListboxOption className={styles.listBoxOption} value="hide">
-                  Hide
-                </ListboxOption>
-                <ListboxOption className={styles.listBoxOption} value="delete">
-                  Delete
-                </ListboxOption>
-              </Listbox>
-            </div>
-          </div>
-
-          <div>
-            <div className={styles.leftContainer}>
-              <Markdown body={body} />
+            <div>
+              <div className={styles.leftContainer}>
+                <Markdown body={body} />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
